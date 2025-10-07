2ead26b13f9075d703b3be5d60ce9a993f817f9c
Krishna Tanwar <kstanwar0000@gmail.com>
2025-10-07 19:00:21 +0530

    Day 72 done

---
+++ Day 72/Ques.py
@@ -0,0 +1,8 @@
+# Question : Given a list of rational numbers, find their product.
+
+from fractions import Fraction
+from functools import reduce
+
+def product(fracs):
+    t = reduce(lambda x, y: x * y, fracs)
+    return t.numerator, t.denominator
