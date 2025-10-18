ba826d25edf2fc283b8905a9d2df89c7a7334c95
Krishna Tanwar <kstanwar0000@gmail.com>
2025-10-18 17:18:14 +0530

    Day 83 done

---
+++ Day 83/Ques.py
@@ -0,0 +1,4 @@
+# Ques : Read first and last name, then print Hello {first} {last}! You just delved into python.
+
+def print_full_name(first, last):
+    print(f"Hello {first} {last}! You just delved into python.")
