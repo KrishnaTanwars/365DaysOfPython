070e5938028abc48afc05f1a5750c2871137d515
Krishna Tanwar <kstanwar0000@gmail.com>
2025-10-21 19:22:20 +0530

    Day 86 done

---
+++ Day 86/Ques.py
@@ -0,0 +1,9 @@
+# Ques : Merge two sorted linked lists list1 and list2 into a single sorted linked list and return its head.
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def mergeTwoLists(list1: ListNode, list2: ListNode) -> ListNode:
+    pass
