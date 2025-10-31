c50716b568a9f0ec34a6e516bbff10f16ca345cf
Krishna Tanwar <kstanwar0000@gmail.com>
2025-10-31 14:53:11 +0530

    Day 96 done

---
+++ Day 96/Ques.py
@@ -0,0 +1,8 @@
+# Ques : Print the HackerRank logo using string alignment methods (.ljust, .center, .rjust) for a given thickness.
+
+thickness = int(input()); c = 'H'
+for i in range(thickness): print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+for i in range(thickness+1): print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+for i in range((thickness+1)//2): print((c*thickness).center(thickness*6))
+for i in range(thickness+1): print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+for i in range(thickness): print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
