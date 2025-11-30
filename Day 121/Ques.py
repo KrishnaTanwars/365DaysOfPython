1eae80186429d584b820f9bddd333825eddbe184
Krishna Tanwar <kstanwar0000@gmail.com>
2025-11-30 20:41:37 +0530

    DAY 121 DONE

---
+++ Day 121/Ques.py
@@ -0,0 +1,7 @@
+# Ques : Time Delta (Difference between two timestamps)
+from datetime import datetime
+fmt="%a %d %b %Y %H:%M:%S %z"
+for _ in range(int(input())):
+    t1=datetime.strptime(input(),fmt)
+    t2=datetime.strptime(input(),fmt)
+    print(int(abs((t1-t2).total_seconds())))
